31fd4a3bd29c97ad3f2dc90f62a6b0e995225875
Eda Dursun <edadrsn929@gmail.com>
2026-02-16 22:21:48 +0300

    Habit saving and habit retrieval functions have been implemented.

--- app/src/main/java/com/edadursun/habitify/data/HabitRemoteDataSource.kt
+++ app/src/main/java/com/edadursun/habitify/data/HabitRemoteDataSource.kt
@@ -2,21 +2,56 @@ package com.edadursun.habitify.data
 
 import com.edadursun.habitify.model.Habit
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObjects
 
-// BU SINIF HABITIN NASIL KAYDEDİLECEĞİNİ BİLİR
+// BU SINIF HABITIN NASIL KAYDEDİLECEĞİNİ BİLİR - Habit verisi firebase e nasıl yazılır ve nasıl okunur
 class HabitRemoteDataSource(private val firestore:FirebaseFirestore) {
 
     //Habiti kaydet
     fun saveHabit(
         userId:String,
-        habitId:String,
-        habit:Habit
+        habit:Habit,
+        onSuccess: () -> Unit,
+        onError: (Throwable) -> Unit
     ){
-        firestore
+        val docRef=firestore
             .collection("users")
             .document(userId)
             .collection("habits")
-            .document(habitId)
-            .set(habit)
+            .document()
+
+        val habitWithId = habit.copy(id = docRef.id)
+
+        docRef.set(habitWithId)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { onError(it) }
+    }
+
+
+    //Kullanıcının habitlerini getir
+    fun getUserHabits(
+        userId: String?,
+        onResult: (List<Habit>) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        if (userId.isNullOrBlank()) {
+            onResult(emptyList())
+            return
+        }
+
+        firestore.collection("users")
+            .document(userId)
+            .collection("habits")
+            .addSnapshotListener{ snapshot, error ->
+
+                if (error != null) {
+                    onError(error)
+                    return@addSnapshotListener
+                }
+
+                //dokümanı habite dönüştürür
+                val habits = snapshot?.toObjects(Habit::class.java) ?: emptyList()
+                onResult(habits)
+            }
     }
 }
--- app/src/main/java/com/edadursun/habitify/data/HabitRepository.kt
+++ app/src/main/java/com/edadursun/habitify/data/HabitRepository.kt
@@ -1,21 +1,83 @@
 package com.edadursun.habitify.data
 
+import android.util.Log
 import com.edadursun.habitify.model.Habit
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.toObject
 import java.util.UUID
 
 //HABIT HANGİ USERA KAYDEDİLECEK
 class HabitRepository(
     private val remoteDataSource: HabitRemoteDataSource,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val firestore:FirebaseFirestore
 ) {
-    fun addHabit(habit: Habit){
+    //Habiti ilgili usera kaydet
+    fun addHabit(
+        habit: Habit,
+        onSuccess: () -> Unit,
+        onError: (Throwable) -> Unit
+    ){
         val userId=userRepository.getOrCreateUserId()
-        val habitId=UUID.randomUUID().toString()
 
         remoteDataSource.saveHabit(
             userId=userId,
-            habitId=habitId,
-            habit=habit
+            habit=habit,
+            onSuccess = onSuccess,
+            onError = onError
         )
+    }
+
+    //Habitin lastUpdated alanını bugünün tarihi ile güncelle
+    fun updateAllHabitsLastUpdated(today:String){
+        val userId=userRepository.getOrCreateUserId()
+        firestore
+            .collection("users")
+            .document(userId)
+            .collection("habits")
+            .get()
+            .addOnSuccessListener {snapshot ->
+                snapshot.documents.forEach { doc ->
+                   val lastUpdated=doc.getString("lastUpdated") ?: ""
+
+                    if(lastUpdated != today){
+                        doc.reference.update("lastUpdated",today)
+                    }
+                }
+            }
+    }
+
+
+    //User idye göre Habitleri getir
+    fun getHabits(onResult: (List<Habit>) -> Unit){
+        val userId=userRepository.getOrCreateUserId()
+        remoteDataSource.getUserHabits(
+            userId = userId,
+            onResult = onResult,
+            onError = {
+                Log.e("HABIT", it.message ?: "Error")
+                onResult(emptyList())
+            }
+        )
+    }
+
+
+    //1 tane Habit getir
+    fun getHabit(
+        habitId:String,
+        onResult:(Habit?) -> Unit
+    ){
+        val userId=userRepository.getOrCreateUserId()
+        firestore
+            .collection("users")
+            .document(userId)
+            .collection("habits")
+            .document(habitId)
+            .get()
+            .addOnSuccessListener { doc ->
+                val habit = doc.toObject(Habit::class.java)
+                onResult(habit)
+            }
+
     }
 }

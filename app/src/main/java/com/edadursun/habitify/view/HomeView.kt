b6d52f0ba397b50b8102cad162acb949570cb9e8
Eda Dursun <edadrsn929@gmail.com>
2026-02-17 21:45:02 +0300

    The header has been configured, and the generated habits have been retrieved and displayed.

--- app/src/main/java/com/edadursun/habitify/view/HomeView.kt
+++ app/src/main/java/com/edadursun/habitify/view/HomeView.kt
@@ -1,21 +1,120 @@
 package com.edadursun.habitify.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.edadursun.habitify.R
+import com.edadursun.habitify.databinding.ActivityHomeViewBinding
+import com.edadursun.habitify.view.adapter.HabitAdapter
+import com.edadursun.habitify.viewmodel.HomeViewModel
 
 class HomeView : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeViewBinding
+
+    // Home ekranına ait ViewModel
+    private lateinit var viewModel: HomeViewModel
+    private lateinit var adapter: HabitAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home_view)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityHomeViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // ViewModel'i Activity scope'unda oluştur
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
+
+        adapter = HabitAdapter(
+            onClick = { habit ->
+                viewModel.onHabitDetailClicked(habit)
+            })
+
+        viewModel.goToHabitDetail.observe(this){ habitId ->
+            val intent=Intent(this,HabitDetailView::class.java)
+            intent.putExtra("habitId",habitId)
+            startActivity(intent)
+
         }
+
+        binding.habitsRecyclerView.layoutManager = LinearLayoutManager(this)
+        binding.habitsRecyclerView.adapter = adapter
+
+
+
+        viewModel.habits.observe(this) { habits ->
+            if (habits.isEmpty()) {
+                binding.homeInfo.visibility = View.VISIBLE
+                binding.habitsRecyclerView.visibility = View.GONE
+            } else {
+                binding.homeInfo.visibility = View.GONE
+                binding.habitsRecyclerView.visibility = View.VISIBLE
+                adapter.updateList(habits)
+            }
+        }
+
+        viewModel.loadHabits()
+
+
+
+        // Create Habit butonuna basılınca ViewModel'e sadece "olay oldu" bilgisini gönderiyoruz
+        binding.createHabitBtn.setOnClickListener {
+            viewModel.onAddHabitClicked()
+        }
+
+        // AddHabit sayfasına gitme event'ini dinle Event gelince navigation işlemi burada yapılır
+        viewModel.goToAddHabit.observe(this) {
+            startActivity(Intent(this, AddHabitView::class.java))
+        }
+
+        // Settings ikonuna basılınca
+        binding.goSettingView.setOnClickListener {
+            viewModel.onSettingsClicked()
+        }
+
+        // Settings sayfasına gitme event'ini dinle
+        viewModel.goToSettings.observe(this) {
+            startActivity(Intent(this, SettingView::class.java))
+        }
+
+
+        // DATE
+        viewModel.todayDate.observe(this) { date ->
+            binding.todayDateText.text = date
+        }
+
+        viewModel.weekDays.observe(this) { days ->
+
+            binding.dayText1.text = days[0].dayName
+            binding.dayNum1.text = days[0].dayNumber
+
+            binding.dayText2.text = days[1].dayName
+            binding.dayNum2.text = days[1].dayNumber
+
+            binding.dayText3.text = days[2].dayName
+            binding.dayNum3.text = days[2].dayNumber
+
+            // BUGÜN
+            binding.dayText4.text = days[3].dayName
+            binding.dayNum4.text = days[3].dayNumber
+
+            binding.dayText5.text = days[4].dayName
+            binding.dayNum5.text = days[4].dayNumber
+
+            binding.dayText6.text = days[5].dayName
+            binding.dayNum6.text = days[5].dayNumber
+
+            binding.dayText7.text = days[6].dayName
+            binding.dayNum7.text = days[6].dayNumber
+        }
+
     }
 }
--- app/src/main/java/com/edadursun/habitify/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/edadursun/habitify/viewmodel/HomeViewModel.kt
@@ -1,10 +1,42 @@
 package com.edadursun.habitify.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.edadursun.habitify.data.HabitRemoteDataSource
+import com.edadursun.habitify.data.HabitRepository
+import com.edadursun.habitify.model.DayItem
+import com.edadursun.habitify.data.UserLocaleDataSource
+import com.edadursun.habitify.data.UserRepository
+import com.edadursun.habitify.model.Habit
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
-class HomeViewModel : ViewModel() {
+class  HomeViewModel(application:Application) : AndroidViewModel(application) {
+
+    private val firestore= FirebaseFirestore.getInstance()
+    private val userRepository = UserRepository(UserLocaleDataSource(application))
+    private val habitRepository = HabitRepository(
+        remoteDataSource = HabitRemoteDataSource(firestore),
+        userRepository = userRepository,
+        firestore = firestore
+    )
+
+    private val _habits=MutableLiveData<List<Habit>>()
+    val habits:LiveData<List<Habit>> = _habits
+
+    // HABITLERI YÜKLE
+    fun loadHabits(){
+       habitRepository.getHabits { habitList ->
+           _habits.postValue(habitList)
+       }
+    }
+
+
 
     //Unit = veri taşımıyoruz, sadece "bir olay oldu" bilgisini veriyoruz, ViewModel, View'e "AddHabit sayfasına git" event'i üretir
     private val _goToAddHabit = MutableLiveData<Unit>()
@@ -12,19 +44,71 @@ class HomeViewModel : ViewModel() {
     // View (Activity), bu LiveData'yı observe eder Dışarıdan sadece okunabilir (encapsulation)
     val goToAddHabit: LiveData<Unit> = _goToAddHabit
 
-
-    // Ayarlar sayfasına gitmek için ayrı bir event Çünkü her event farklı bir aksiyon tetikler
-    private val _goToSettings = MutableLiveData<Unit>()
-    val goToSettings: LiveData<Unit> = _goToSettings
-
-
     // Create Habit (+) butonuna basıldığında çağrılır ViewModel "AddHabit ekranına git" olayını tetikler
     fun onAddHabitClicked() {
         _goToAddHabit.value = Unit
     }
 
+
+    // Ayarlar sayfasına gitmek için ayrı bir event Çünkü her event farklı bir aksiyon tetikler
+    private val _goToSettings = MutableLiveData<Unit>()
+    val goToSettings: LiveData<Unit> = _goToSettings
+
     // Ayarlar ikonuna basıldığında çağrılır ViewModel "Settings ekranına git" olayını tetikler
     fun onSettingsClicked() {
         _goToSettings.value = Unit
     }
+
+    // Habit detayına git
+    private val _goToHabitDetail=MutableLiveData<String>()
+    val goToHabitDetail : LiveData<String> = _goToHabitDetail
+
+    fun onHabitDetailClicked(habit:Habit){
+        _goToHabitDetail.value=habit.id
+    }
+
+
+
+    // DATE
+    // Güncel tarihi tutar
+    private val _todayDate=MutableLiveData<String>()
+    val todayDate:LiveData<String> = _todayDate
+
+    //Bugünün 3 gün öncesi + bugünün kendisi + 3 gün sonrası olmak üzere toplam 7 gün tutar
+    private val _weekDays=MutableLiveData<List<DayItem>>()
+    val weekDays:LiveData<List<DayItem>> = _weekDays
+
+    init{
+        setTodayDate() // viewmodel ilk oluşturulduğunda bugünün tarihini ayarla
+        setWeekDaysAroundToday() //bugünden itibaren 7 gün hesapla ve listeyi doldur
+    }
+
+
+    fun setTodayDate(){
+        // cihazın bugünkü tarihini al
+        val sdf = SimpleDateFormat("d , MMMM", Locale.ENGLISH)
+        _todayDate.value = sdf.format(Date())
+    }
+
+    fun setWeekDaysAroundToday() {
+        val calendar = Calendar.getInstance()
+        val formatter = SimpleDateFormat("EEE", Locale.ENGLISH)
+
+        val list = mutableListOf<DayItem>()
+
+        for (i in -3..3) {
+            val temp = calendar.clone() as Calendar
+            temp.add(Calendar.DAY_OF_YEAR, i)
+
+            list.add(
+                DayItem(
+                    dayName = formatter.format(temp.time).uppercase(),
+                    dayNumber = temp.get(Calendar.DAY_OF_MONTH).toString()
+                )
+            )
+        }
+
+        _weekDays.value = list
+    }
+
 }
